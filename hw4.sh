8a8e054a619fe262f653ac51da45bfda65ed50f8
syd-gav <smgavula@yahoo.com>
2024-09-21 20:35:34 -0400

    Script to download and view GCF information
    Will allow for the download of GCF information and creation of a gene file to visualize on IGV

---
+++ hw4.sh
@@ -0,0 +1,27 @@
+#This script will allow the download of a GCF file and produce genome files to view in IGV
+
+#Set the variables as below
+
+# The accession ID of the genome
+GCF=GCF_000195955.2
+
+# The zip file following download
+ZIP=ncbi_dataset.zip
+
+# The unziped gff file
+gff=ncbi_dataset/data/GCF_000195955.2/genomic.gff
+
+# The name of the new gene separate gff file
+new=ncbi_dataset/data/GCF_000195955.2/gene.gff
+
+# Download the genome and annotation file using the accession ID
+datasets download genome accession ${GCF} --include gff3,cds,protein,rna,genome
+
+# Unzip the file that was downloaded
+unzip ${ZIP}
+
+# Look at the first 10 lines that do not have # to ensure proper download
+cat ${gff} | grep '#' | head
+
+# Put all of the lines that are annotated as a 'gene' into it's own gff file
+cat ${gff} | awk ' $3=="gene" {print $0}' > ${new}
